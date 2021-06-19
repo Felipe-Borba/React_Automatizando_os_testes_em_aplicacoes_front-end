22f39aebfd3dd3c6893a7e3fbe082b0ba0348ccc
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-19 17:24:22 -0300

    03-04 react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
@@ -24,34 +24,54 @@ describe("Componente principal", () => {
   });
 
   describe("Quando eu realizo uma transação", () => {
-    test("que é um saque com valor menor que o saldo, o valor vai diminuir", () => {
-      const valores = {
-        transacao: "saque",
-        valor: 50,
-      };
-      const novoSaldo = calcularNovoSaldo(valores, 150);
+    describe("que é um saque", () => {
+      test("com valor menor que o saldo, o valor vai diminuir", () => {
+        const valores = {
+          transacao: "saque",
+          valor: 50,
+        };
+        const novoSaldo = calcularNovoSaldo(valores, 150);
 
-      expect(novoSaldo).toBe(100);
+        expect(novoSaldo).toBe(100);
+      });
+
+      test("com valor maior que o saldo, o valor vai ficar negativo", () => {
+        const valores = {
+          transacao: "saque",
+          valor: 250,
+        };
+        const novoSaldo = calcularNovoSaldo(valores, 150);
+
+        expect(novoSaldo).toBe(-100);
+      });
+
+      test("a transação deve ser realizada", () => {
+        const { getByText, getByTestId, getByLabelText } = render(<App />);
+
+        const saldo = getByText("R$ 1000");
+        const transacao = getByLabelText("Saque");
+        const valor = getByTestId("valor");
+        const botaoTransacao = getByText("Realizar operação");
+
+        expect(saldo.textContent).toBe("R$ 1000");
+        fireEvent.click(transacao, { target: { value: "saque" } });
+        fireEvent.change(valor, { target: { value: 10 } });
+        fireEvent.click(botaoTransacao);
+
+        expect(saldo.textContent).toBe("R$ 990");
+      });
     });
 
-    test("que é um saque com valor maior que o saldo, o valor vai ficar negativo", () => {
-      const valores = {
-        transacao: "saque",
-        valor: 250,
-      };
-      const novoSaldo = calcularNovoSaldo(valores, 150);
+    describe("que é um deposito", () => {
+      test("com valor positivo, o valor vai aumentar", () => {
+        const valores = {
+          transacao: "deposito",
+          valor: 50,
+        };
+        const novoSaldo = calcularNovoSaldo(valores, 150);
 
-      expect(novoSaldo).toBe(-100);
-    });
-
-    test("que é um deposito, o valor vai aumentar", () => {
-      const valores = {
-        transacao: "deposito",
-        valor: 50,
-      };
-      const novoSaldo = calcularNovoSaldo(valores, 150);
-
-      expect(novoSaldo).toBe(200);
+        expect(novoSaldo).toBe(200);
+      });
     });
   });
 });
